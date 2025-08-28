d72b6d585ef8d77cceb6ed9cd390eea5022b14f3
ManasLalwaniRev <Manas.Lalwani@revolvespl.com>
2025-08-28 11:07:18 -0400

    Server

--- src/components/UserProfilePage.jsx
+++ src/components/UserProfilePage.jsx
@@ -707,7 +707,7 @@ const UserProfilePage = ({ setCurrentPage, currentUserId, currentUsername, curre
                     <tr>
                       <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Username</th>
                       <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+                      {/* <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th> */}
                     </tr>
                   </thead>
                   <tbody className="bg-white divide-y divide-gray-200">
